7d85726e4190ca65e8b4125141ec0337e27da6f4
Jonatan Ochoa <jonatan.ochoa@udea.edu.co>
2022-10-10 19:00:26 -0500

    Actualizar carpeta del back

--- back/app.js
+++ back/app.js
@@ -1,4 +1,12 @@
 const express=require("express");
-const app=express();
+const app = express();
+
+app.use(express.json());
+
+//importar la ruta
+const productos= require("./routes/products")
+
+//ruta para el navegador
+app.use('/api',productos)
 
 module.exports=app
--- back/server.js
+++ back/server.js
@@ -1,1 +1,8 @@
-const app=require("app")
+const app=require("./app")// Arrancar el servidor
+
+//Setear el archivo de confirguracion 
+const dotenv= require("dotenv");
+const { connect } = require("mongoose");
+dotenv.config({path: 'back/config/config.env'})
+
+connectDatabase();

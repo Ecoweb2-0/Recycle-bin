49d6a7c354b9f04bbf75e81e733515a14d715946
Fabian Andres <sanandres.programa@gmail.com>
2022-10-18 20:55:30 -0500

    Borrando para cofigurar react

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Header from './components/layout/Header';
-import { Footer } from './components/layout/Footer';
-import Home from './components/Home';
-//Router traido desde react-router-dom (no confundir con el de express)
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 
 function App() {
   return (
-    <Router>
     <div className="App">
-        <Header />
-        <div className='container container-fluid'>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/Home" element={<Home />}/>
-          </Routes>
-        </div>
-        <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    </Router>
   );
 }
 

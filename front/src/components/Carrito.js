07b84ac37040f40ba70db0af3ce7c787816880e4
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-10-21 08:36:17 -0500

    ajustes carrito

--- front/src/components/Carrito.js
+++ front/src/components/Carrito.js
@@ -28,21 +28,17 @@ export const Carrito = () => {
             <h1 id="encabezado_productos">Productos Seleccionados</h1>
 
             <section id="productos" className='container mt-5'>
-                <div className='row'>
+                <div className='col'>
                     {productos && productos.map (producto => (
                         <div key={producto._id} className='col-sm-12 col-md-6 col-lg-3 my-3'>
                         <div className='card p-3 rounded'>
                             <img className='card-img-top mx-auto' src={producto.imagen[0].url} alt={producto.imagen[0].public_id}></img>
                             <div className='card-body d-flex flex-column'>
                                 <h5 id="titulo_producto"><Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner' style={{width: `${(producto.calificacion/5)*100}%`}}></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> {producto.numCalificaciones} Reviews</span>
-                                </div>
-                                <p className='card-text'>${producto.precio}</p><Link to={`/producto/${producto._id}`} id="view_btn" className='btn btn-block'>
-                                    Ver detalle
+                                <p className='card-desc'>{producto.descripcion}</p>
+                                <p className='card-text'>${producto.precio}</p>
+                                <Link to={`/producto/${producto._id}`} id="danger">
+                                    Eliminar
                                 </Link>
                             </div>
                         </div>

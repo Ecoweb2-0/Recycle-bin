dfd447db06b9b23846d699f01bfe67d5a4ac5a79
Jonatan Ochoa <jonatan.ochoa@udea.edu.co>
2022-10-22 13:05:49 -0500

    Modifico pantalla de producto

--- front/src/components/products/ProductDetails.js
+++ front/src/components/products/ProductDetails.js
@@ -5,18 +5,38 @@ export const ProductDetails = () => {
   return (
     <Fragment>
         <MetaData title="Cobre tonelada"></MetaData>
-        <div className='row d-flex justify-content-around'>
-            <div className='col-12 col-lg-5 img-fluid' id="imagen_producto">
-                <img src="../images/Cobre_tonelada.jpg"  alt="Imagen_producto" height="450" width="450"></img>
-            </div>
-
-            <div className='col-12 col-lg-5 mt-5'>
-                <h3>Cobre en tonelada</h3>
-                <p id="product_id">Product #3253252</p>
-            </div>
-        </div>
+           <div class="card-footer p-8">
+              <div className='row d-flex left-content-around'>
+                    <img className="card-img-justify" src="../images/Cobre_tonelada.jpg" alt="Imagen_producto" height="300" width="300" />
+                     
+              <div className='position:left '>
+                    <img className="card-img-left" src="../images/Formas.png" alt="Imagen_producto" height="300" width="300" />
+                  </div>        
+                 </div>     
+                      
+                      <div class="card-body p-8">
+                  <div class="text-justify">
+                      </div>
+                            <h5 class="fw-bolder">COBRE POR TONELADA</h5>
+                                  
+                            <div class="d-flex justify-content-justify small text-warning mb-2">
+                            </div> 
+                                <p id="product_id">CADA CONTENEDOR DE CONTIENE 1.000 KILOS DE COBRE 100% RECICLADO</p>                                 
+                                  <div>ANTES:$ ̶2̶0̶.̶0̶0̶0̶.̶0̶0̶0̶</div>
+                                  <div>AHORA:$ 17.000.000</div>
+                            </div>
+                    </div>
+                            
+                      <div class="card-footer p-0 pt-0 border-top-0 bg-transparent">
+                          <div class="padding: 10% 0"><a class="btn btn-outline-dark mt-auto" href="#">Adicicionar a carrito</a></div>
+                      </div>
+              
+               
+                    
     </Fragment>
   )
 }
+
+
 
 

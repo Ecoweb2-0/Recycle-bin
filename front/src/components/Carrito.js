8ca0db44e2b8d20d5ed942e054a7291f1fc31677
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-10-25 22:06:58 -0500

    cuadrar carrito y crear base ventas

--- front/src/components/Carrito.js
+++ front/src/components/Carrito.js
@@ -2,8 +2,8 @@ import React, { Fragment, useEffect } from 'react'
 import MetaData from './layout/MetaData'
 import {useDispatch, useSelector} from 'react-redux'
 import { getProducts } from '../actions/productActions'
-import { Link } from 'react-router-dom'
 import { useAlert} from 'react-alert'
+import { Link } from 'react-router-dom'
 
 export const Carrito = () => {
     const { loading, productos, error} = useSelector(state=> state.products)
@@ -24,20 +24,18 @@ export const Carrito = () => {
         <Fragment>
             {loading ? <i className="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
                 <Fragment>
+                <div className="col-9 text-center">
                     <MetaData title="Excelente eleccion"></MetaData>
-                    <h3>Productos Seleccionados</h3>
-
-            <section id="productos">
-
-            <table className="table col-11">
-                <thead className="table-dark">
+                <br></br>
+                <h3>Carrito de compras</h3>
+               <table className="table">
+                <thead className="table-az">
                     <tr>
                         <th className='col-1 text-center'></th>
                         <th className='col-1 text-center'></th>
-                        <th className='col-3'>Producto</th>
+                        <th className='col-3 text-left'>Producto</th>
                         <th className='col-1 text-center'>Cantidad</th>
-                        <th className='col-2 text-center'>Vr. Unit</th>
-                        <th className='col-2 text-center'>Vr. Total</th>
+                        <th className='col-2 text-right'>Vr. Unit</th>
                         <th className='col-1 text-center'></th>
                     </tr>
                 </thead>
@@ -48,21 +46,22 @@ export const Carrito = () => {
                             <input type="checkbox" name="" id="seleccion" />
                         </td>
                         <td className='col-1'> <img className='card-img-min rounded mx-auto d-block' src={producto.imagen[0].url} alt={producto.imagen[0].public_id} ></img></td>
-                        <td className="col-3">{producto.nombre}</td>
+                        <td className="col-3 text-left">{producto.nombre}</td>
                         <td className="col-1 text-center">1</td>
                         <td className="col-2 text-right">$ {producto.precio}</td>
-                        <td className="col-2 text-right">$ {producto.precio}</td>
-                        <td><Link to={`/producto/${producto._id}`} id="delete_cart_item"clasName="col-1 text-center">X</Link></td>
+                        <td><Link to={`/producto/${producto._id}`}><img src='./images/basura.jpg' className='basur'></img></Link></td>
                     </tr>
                 ))}
                 </tbody>
             </table> 
-            </section>
+            
             <div> <h3 className="col-9 text-right">Total</h3></div> 
             <div className='col-11 text-center'><button  className='btn btn-danger text-center'>Ir a pagar</button></div>
+            </div>
                 </Fragment>
 
             )}
+            
             
 
         </Fragment>

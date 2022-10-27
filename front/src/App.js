96f122027e82aa57e8b5f69dc12eb3ed8cb6158e
Emmanuel Rivera Suarez <mancisnegas@gmail.com>
2022-10-27 16:08:43 -0500

    Interfaz editar productos

--- front/src/App.js
+++ front/src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import React from 'react';
 import Header from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
-import Home from './components/Home';
+import {Home} from './components/Home';
+import { ProductDetails } from './components/products/ProductDetails';
 //Router traido desde react-router-dom (no confundir con el de express)
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import {Carrito} from './components/Carrito';
+import { Transacciones } from './components/Transacciones';
+import { AdminProduct } from './components/AdminProduct';
+import AdminProduct2 from './components/AdminProduct2';
 
 function App() {
   return (
@@ -16,6 +20,11 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/Home" element={<Home />}/>
+            <Route path="/Carrito" element={<Carrito />}/>
+            <Route path="/ventas" element={<Transacciones />}/>
+            <Route path="/producto/:id" element={<ProductDetails />}/>
+            <Route path="/Admin-product" element={<AdminProduct />}/>
+
           </Routes>
         </div>
         <Footer />

65458a5c4f0e0f7c41adbcfc1240124306003e22
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-11-02 13:29:10 -0500

    Configuración de usuarios y logOut

---
+++ back/utils/sendEmail.js
@@ -0,0 +1,22 @@
+const nodemailer= require("nodemailer")
+
+const sendEmail = async options =>{
+    const transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "4d79688cf16eac",
+          pass: "7ac98b348aa423"
+        }
+      });
+    const mensaje={
+        from: "Recycle_Bin Store <noreply@recyclebin.com>",
+        to: options.email,
+        subject: options.subject,
+        text: options.mensaje
+    }
+
+    await transport.sendMail(mensaje)
+}
+
+module.exports= sendEmail;

df431087931ad6e4349281f2181843a1b936484a
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-11-09 14:56:06 -0500

    Ajustes en productList

--- front/src/components/admin/ProductsList.js
+++ front/src/components/admin/ProductsList.js
@@ -86,7 +86,7 @@ export const ProductsList = () => {
                     <Fragment>
                         <h1 className="my-5">Productos Registrados</h1>
 
-                        {loading ? <span className='loader'></span> :(
+                        {loading ? <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
                             <MDBDataTable
                                 data={setProducts()}
                                 className="px-3 text-center"

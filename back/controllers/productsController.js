1b14bec31d0cb2901c1ca854290bc93a9886c136
Felipe Fonseca <115054329+zarco98@users.noreply.github.com>
2022-11-10 18:22:55 -0500

    Rama actualizada al 10/11/2022

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,97 +1,190 @@
-const producto=require("../models/productos")
-const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url));
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const producto = require("../models/productos");
+const APIFeatures = require("../utils/apiFeatures");
+const ErrorHandler = require("../utils/errorHandler");
+const fetch = (url) => import('node-fetch').then(({ default: fetch }) => fetch(url)); //Usurpación del require
 
 //Ver la lista de productos
-exports.getProducts=async (req,res,next) =>{
-    const productos=await producto.find();
+exports.getProducts = catchAsyncErrors(async (req, res, next) => {
+
+    const resPerPage = 4;
+    const productsCount = await producto.countDocuments();
+
+    const apiFeatures = new APIFeatures(producto.find(), req.query)
+        .search()
+        .filter();
+
+    let products = await apiFeatures.query;
+    let filteredProductsCount= products.length;
+    apiFeatures.pagination(resPerPage);
+    products = await apiFeatures.query.clone();
+
     res.status(200).json({
-        success:true,
-        count: productos.length,
-        productos
+        success: true,
+        productsCount,
+        resPerPage,
+        filteredProductsCount,
+        products
     })
-}
 
+})
 
-//Consultar un producto por id
-exports.getProductById= async (req,res,next)=>{
-    const product=await producto.findById(req.params.id)
-    if (!product){
-        return res.status(404).json({
-            success:false,
-            message: "No encontramos ese producto"
-        })
+//Ver un producto por ID
+exports.getProductById = catchAsyncErrors(async (req, res, next) => {
+    const product = await producto.findById(req.params.id)
 
+    if (!product) {
+        return next(new ErrorHandler("Producto no encontrado", 404))
     }
+
     res.status(200).json({
-        success:true,
-        message: "Aqui debajo encunetras información sobre tu producto: ",
+        success: true,
+        message: "Aqui debajo encuentras información sobre tu producto: ",
         product
     })
-}
+})
 
-// Update un producto
-exports.updateProduct= async (req,res,next) =>{
-    let product=await producto.findById(req.params.id)
-    if (!product){ //Si el producto noe stá finaliza el proceso
-            return res.status(404).json({
-                success:false,
-                message: "No encontramos ese producto"
-            })
+//Update un producto
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+    let product = await producto.findById(req.params.id) //Variable de tipo modificable
+    if (!product) {
+        return next(new ErrorHandler("Producto no encontrado", 404))
     }
-// Si el productó está lo actualiza
-    product= await producto.findByIdAndUpdate(req.params.id, req.body,{
-        new:true, // valida sólo lo nuevo
-        runValidators:true
+
+    //Si el objeto si existia, entonces si ejecuto la actualización
+    product = await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new: true, //Valido solo los atributos nuevos o actualizados
+        runValidators: true
     });
-    //Confirma que si actualizó
+    //Respondo Ok si el producto si se actualizó
     res.status(200).json({
-        success:true,
-        message:"Producto actualizado correctamente",
-        producto
-    })
-}
-
-//Eliminar producto
-exports.deleteProduct= async (req,res,next) =>{
-    const product=await producto.findById(req.params.id)
-    if (!product){ //Si el producto no está finaliza el proceso
-            return res.status(404).json({
-                success:false,
-                message: "No encontramos ese producto"
-            })
-        }
-        await product.remove();
-        res.status(200).json({
-            success:true,
-            message: "El producto fue eliminado"
-        })
-}
-
-//Crear nuevo producto /api/producto/nuevo
-exports.newProduct=async(req,res,next)=>{
-    const product=await producto.create(req.body);
-    
-    res.status(201).json({
-        success:true,
+        success: true,
+        message: "Producto actualizado correctamente",
         product
     })
-}
+})
 
-//Usando fetchall
+
+//Eliminar un producto
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+    const product = await producto.findById(req.params.id) //Variable de tipo modificable
+
+    if (!product) {
+        return next(new ErrorHandler("Producto no encontrado", 404))
+    }
+
+    await product.remove();//Eliminamos el proceso
+    res.status(200).json({
+        success: true,
+        message: "Producto eliminado correctamente"
+    })
+})
+
+//Crear nuevo producto /api/productos
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+    req.body.user = req.user.id;
+    const product = await producto.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+})
+
+
+//Crear o actualizar una review
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+    const { rating, comentario, idProducto } = req.body;
+
+    const opinion = {
+        nombreCliente: req.user.nombre,
+        rating: Number(rating),
+        comentario
+    }
+
+    const product = await producto.findById(idProducto);
+
+    const isReviewed = product.opiniones.find(item =>
+        item.nombreCliente === req.user.nombre)
+
+    if (isReviewed) {
+        product.opiniones.forEach(opinion => {
+            if (opinion.nombreCliente === req.user.nombre) {
+                opinion.comentario = comentario,
+                    opinion.rating = rating
+            }
+        })
+    } else {
+        product.opiniones.push(opinion)
+        product.numCalificaciones = product.opiniones.length
+    }
+
+    product.calificacion = product.opiniones.reduce((acc, opinion) =>
+        opinion.rating + acc, 0) / product.opiniones.length
+
+    await product.save({ validateBeforeSave: false });
+
+    res.status(200).json({
+        success: true,
+        message: "Hemos opinado correctamente"
+    })
+
+})
+
+//Ver todas las review de un producto
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
+    const product = await producto.findById(req.query.id)
+
+    res.status(200).json({
+        success: true,
+        opiniones: product.opiniones
+    })
+})
+
+//Eliminar review
+exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+    const product = await producto.findById(req.query.idProducto);
+
+    const opi = product.opiniones.filter(opinion =>
+        opinion._id.toString() !== req.query.idReview.toString());
+
+    const numCalificaciones = opi.length;
+
+    const calificacion = opi.reduce((acc, Opinion) =>
+        Opinion.rating + acc, 0) / opi.length;
+
+    await producto.findByIdAndUpdate(req.query.idProducto, {
+        opi,
+        calificacion,
+        numCalificaciones
+    }, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+    res.status(200).json({
+        success: true,
+        message: "review eliminada correctamente"
+    })
+
+})
+
+//HABLEMOS DE FETCH
 //Ver todos los productos
-function verProductos(){
-    fetch('http://localhost:4000/api/productos')
-    .then(res=>res.json())
-    .then(res=>console.log(res))
-    .catch(err=>console.error(err))
+function verProductos() {
+    fetch('http://127.0.0.1:4000/api/productos')
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err))
 }
-//verProductos(); Se llama al metodo creado para probar consulta
 
-//Ver producto por id
-function verProductoPorID(id){
-    fetch('http://localhost:4000/api/producto/'+id)
-    .then(res=>res.json())
-    .then(res=>console.log(res))
-    .catch(err=>console.error(err))
+//verProductos(); LLamamos al metodo creado para probar la consulta
+
+//Ver por id
+function verProductoPorID(id) {
+    fetch('http://127.0.0.1:4000/api/producto/' + id)
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(err => console.error(err))
 }
-//verProductoPorID('6348695684579a6e95bf8b12'); probar el fetchall para busqueda por id
+
+//verProductoPorID('63456a8d9163cb9dbbcaa235'); Probamos el metodo con un id
---
+++ front/src/actions/productActions.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+import { 
+    ALL_PRODUCTS_REQUEST,
+    ALL_PRODUCTS_SUCCESS,
+    ALL_PRODUCTS_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
+    CLEAR_ERRORS
+} from '../constants/productConstants';
+
+// Acciones para obtener el listado completo de productos, habilitado para aplicar filtros
+export const getProducts = (currentPage = 1, keyword = '', precio) => async (dispath) => {
+    try{
+        dispath({type: ALL_PRODUCTS_REQUEST});
+        
+        // Cargar la info de los productos en la variable data que depende de los filtros que se hagan
+        let link = `/api/productos?keyword=${keyword}&page=${currentPage}&precio[gte]=${precio[0]}&precio[lte]=${precio[1]}`
+        const {data} = await axios.get(link);
+
+        dispath({
+            type: ALL_PRODUCTS_SUCCESS,
+            payload: data
+        });
+    }   catch(error){
+        dispath({
+            type: ALL_PRODUCTS_FAIL,
+            payload: error.response.data.message
+        });
+    }
+}
+
+// Acciones para obtener el detalle de un producto
+export const getProductDetails = (id) => async (dispath) => {
+    try{
+        dispath({type: PRODUCT_DETAILS_REQUEST});
+        // Cargar la info de los productos en la variable data
+        const {data} = await axios.get(`/api/producto/${id}`);
+        dispath({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        });
+    }   catch(error){
+        dispath({
+            type:PRODUCT_DETAILS_FAIL,
+            payload: error.response.data.message
+        });
+    }
+}
+
+// Limpiar los errores
+export const clearErrors = () => async(dispath) => {
+    dispath({
+        type: CLEAR_ERRORS
+    });
+}

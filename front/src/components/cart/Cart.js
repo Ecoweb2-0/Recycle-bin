cfe8dca0dff3ce7574bd3b074a79b7f7036fd6e2
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-11-13 19:42:01 -0500

    reverso del carrito

--- front/src/components/cart/Cart.js
+++ front/src/components/cart/Cart.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useState} from 'react'
 import { Link } from 'react-router-dom'
 import MetaData from '../layout/MetaData'
-let {cartItems} = require ('../../agregarItems.js')
 
 
 const Cart = () => {
@@ -20,9 +19,33 @@ const Cart = () => {
       setQuantity(qty)
    }
 
+    //Json de ejemplo
+let cartItems=[
+        {
+            "_id": "6351a0ac9dc6821674880d59",
+            "nombre": "Plástico PET Cristal",
+            "precio": 2815000,
+            "imagen": "./images/PETCristal.jpg",
+            "inventario": 20,
+        },
+        {
+            "_id": "63519e876757fd7f1330ca64",
+            "nombre": "Aluminio por tonelad",
+            "precio": 5111000,
+            "imagen": "./images/Aluminio.jpg",
+            "inventario": 5,
+        },
+        {
+            "_id": "63519fc16757fd7f1330ca67",
+            "nombre": "Plástico Rígido por Tonelada",
+            "precio": 1911000,
+            "imagen": "./images/PlasticoRigido.jpg",
+            "inventario": 3,
+        },
+    
+    ]
 
-
-let cartItems = Array.from(cartItems);
+cartItems = Array.from(cartItems);
 
     return (
         <Fragment>
@@ -69,7 +92,7 @@ let cartItems = Array.from(cartItems);
                                             <div className="col-4 col-lg-1 mt-4 mt-lg-0">
                                                 <i id="delete_cart_item" className="fa fa-trash btn btn-danger" ></i>
                                             </div>
-                                            
+
                                         </div>
                                     </div>
                                     <hr />
@@ -77,7 +100,6 @@ let cartItems = Array.from(cartItems);
                             
                         ))}
                         </div>
-                        
 
                         <div className="col-12 col-lg-3 my-4">
                             <div id="order_summary">

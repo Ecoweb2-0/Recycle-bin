fde3e346b6d15ff482da83ca864e3af6cd578ab4
Caritoleal <109706077+Caritoleal@users.noreply.github.com>
2022-11-16 22:50:22 -0500

    Ajuste en rutas de orden

--- back/routes/orders.js
+++ back/routes/orders.js
@@ -16,7 +16,7 @@ router.route("/order/:id").get(isAuthenticatedUser, getOneOrder)
 router.route("/orders/me").get(isAuthenticatedUser, myOrders)
 
 //rutas de admin
-router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), allOrders)
+router.route("/admin/ordenes").get(isAuthenticatedUser, authorizeRoles("admin"), allOrders)
 router.route("/admin/order/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
 router.route("/admin/order/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder)
 
